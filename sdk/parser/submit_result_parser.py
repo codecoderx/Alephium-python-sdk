db83abce07013b952ae04ae72a2e7568dffda743
wangdi <663248062@qq.com>
2024-03-06 14:14:33 +0800

    fix: Update error slice index

--- sdk/parser/submit_result_parser.py
+++ sdk/parser/submit_result_parser.py
@@ -15,15 +15,15 @@ class SubmitResultParser:
     def parse(self):
         result = {}
         self.end_offset = self.start_offset + self.fromGroup_size
-        result["fromGroup"] = util.parse_be_uint32(self.buffer[self.start_offset, self.end_offset])
+        result["fromGroup"] = util.parse_be_uint32(self.buffer[self.start_offset:self.end_offset])
 
         self.start_offset += self.fromGroup_size
         self.end_offset = self.start_offset + self.toGroup_size
-        result["toGroup"] = util.parse_be_uint32(self.buffer[self.start_offset, self.end_offset])
+        result["toGroup"] = util.parse_be_uint32(self.buffer[self.start_offset:self.end_offset])
 
         self.start_offset += self.toGroup_size
         self.end_offset = self.start_offset + self.result_size
 
-        result["result"] = util.parse_be_uint8(self.buffer[self.start_offset, self.end_offset])
+        result["result"] = util.parse_be_uint8(self.buffer[self.start_offset:self.end_offset])
 
         return result
